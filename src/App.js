5697bf6260be969957cfc176de0e9b070121c13a
jonathanhhoward <2773262@gmail.com>
2020-09-05 15:34:31 -0400

    Fix anchor warning

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ function App() {
         <header>
           <nav className="navbar">
             <ul className="menu">
-              <li><a href="#home">Home</a></li>
-              <li><a href="#projects">Projects</a></li>
-              <li><a href="#profiles">Profiles</a></li>
+              <li><a href={"#home"}>Home</a></li>
+              <li><a href={"#projects"}>Projects</a></li>
+              <li><a href={"#profiles"}>Profiles</a></li>
             </ul>
           </nav>
         </header>
