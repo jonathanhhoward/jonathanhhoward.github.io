d52b20bcd23b73fb82333c483ffc5f5a713c5ae1
jonathanhhoward <2773262@gmail.com>
2020-09-05 15:44:12 -0400

    Updates

---
+++ precache-manifest.8849d26e0aefbf2b089c0040e750d5c3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115ab24f66d9ea0f7fe43bbb35b7117c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751560e7a5b9fe22de1d",
+    "url": "/static/css/main.b5b9518f.chunk.css"
+  },
+  {
+    "revision": "c0624358a6205ba44c11",
+    "url": "/static/js/2.324bff06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.324bff06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751560e7a5b9fe22de1d",
+    "url": "/static/js/main.3e8690b4.chunk.js"
+  },
+  {
+    "revision": "03b3d310c076805652fd",
+    "url": "/static/js/runtime-main.fa3c400e.js"
+  },
+  {
+    "revision": "b05da2685abaef23d41c438c5a7abd07",
+    "url": "/static/media/analog-clock.b05da268.png"
+  },
+  {
+    "revision": "3e96157e319d31ef60b3fc4968f66e28",
+    "url": "/static/media/bar-chart.3e96157e.png"
+  },
+  {
+    "revision": "ae46569837df355da8de149ece440226",
+    "url": "/static/media/exercise-tracker.ae465698.png"
+  },
+  {
+    "revision": "093f8f6576234b7c0fd67e276fea86d6",
+    "url": "/static/media/file-metadata.093f8f65.png"
+  },
+  {
+    "revision": "15fde8926d89c15aaada544b61540948",
+    "url": "/static/media/javascript-calculator.15fde892.png"
+  },
+  {
+    "revision": "2e20f747e94a7dbfc38e061f9cf74510",
+    "url": "/static/media/markdown-previewer.2e20f747.png"
+  },
+  {
+    "revision": "e044f6e8ea4aa4738999aee770ec3761",
+    "url": "/static/media/metric-imp-converter.e044f6e8.png"
+  },
+  {
+    "revision": "05eaf99337c54c631a1136351a807f33",
+    "url": "/static/media/montserrat-all-400-italic.05eaf993.woff"
+  },
+  {
+    "revision": "7bf99c007acd1baa1f21903b6fda4d65",
+    "url": "/static/media/montserrat-all-400-normal.7bf99c00.woff"
+  },
+  {
+    "revision": "8ecda6d98ca5ad1b6e0401a1a83af113",
+    "url": "/static/media/montserrat-cyrillic-400-italic.8ecda6d9.woff2"
+  },
+  {
+    "revision": "298b48e6dfdf7ec4ef8907ff8a7182b1",
+    "url": "/static/media/montserrat-cyrillic-400-normal.298b48e6.woff2"
+  },
+  {
+    "revision": "a98ab76ed6cb304ef2d3fe37c52ee0c5",
+    "url": "/static/media/montserrat-cyrillic-ext-400-italic.a98ab76e.woff2"
+  },
+  {
+    "revision": "a13ef88d6cc2bace52bc1684ecb8823d",
+    "url": "/static/media/montserrat-cyrillic-ext-400-normal.a13ef88d.woff2"
+  },
+  {
+    "revision": "5cad650422a7184467af5a4d17b264c4",
+    "url": "/static/media/montserrat-latin-400-italic.5cad6504.woff2"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/static/media/montserrat-latin-400-normal.bc3aa95d.woff2"
+  },
+  {
+    "revision": "04cea7a0f9476b46e814e03c6688d59b",
+    "url": "/static/media/montserrat-latin-ext-400-italic.04cea7a0.woff2"
+  },
+  {
+    "revision": "97638b9a50d86ae960e84043314bf07a",
+    "url": "/static/media/montserrat-latin-ext-400-normal.97638b9a.woff2"
+  },
+  {
+    "revision": "13e3fb656a7f00b1c0251eee6f8cc60b",
+    "url": "/static/media/montserrat-vietnamese-400-italic.13e3fb65.woff2"
+  },
+  {
+    "revision": "4c16f6b5ec6cbeff86143c0011ad985c",
+    "url": "/static/media/montserrat-vietnamese-400-normal.4c16f6b5.woff2"
+  },
+  {
+    "revision": "c4c993c1d95df05ae3e713f7a2f77d27",
+    "url": "/static/media/pomodoro-clock.c4c993c1.png"
+  },
+  {
+    "revision": "fac3d9dbc8d24565ef25c1c7a6488c51",
+    "url": "/static/media/product-landing-page.fac3d9db.png"
+  },
+  {
+    "revision": "c12ac6cd91e7eb4b71649e494c10ed78",
+    "url": "/static/media/profile-selfie.c12ac6cd.jpg"
+  },
+  {
+    "revision": "eb1d3bab3b3fe384225f06cef043c423",
+    "url": "/static/media/survey-form.eb1d3bab.png"
+  },
+  {
+    "revision": "47e9ecdd9d196d83ac1352cc678cbd86",
+    "url": "/static/media/technical-documentation-page.47e9ecdd.png"
+  }
+]);
