278badc96a162021fc6fe1116c126a77fca7e6f1
jonathanhhoward <2773262@gmail.com>
2020-09-05 15:12:08 -0400

    Import existing images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import './App.scss';
+import profileSelfie from './images/profile-selfie.jpg';
+import surveyForm from './images/survey-form.png';
+import technicalDocumentationPage from './images/technical-documentation-page.png';
+import productLandingPage from './images/product-landing-page.png';
+import markdownPreviewer from './images/markdown-previewer.png';
+import javascriptCalculator from './images/javascript-calculator.png';
+import pomodoroClock from './images/pomodoro-clock.png';
+import barChart from './images/bar-chart.png';
+import exerciseTracker from './images/exercise-tracker.png';
+import fileMetadata from './images/file-metadata.png';
+import metricImpConverter from './images/metric-imp-converter.png';
+import analogClock from './images/analog-clock.png';
 
 function App() {
   return (
@@ -16,7 +28,7 @@ function App() {
         <main>
           <section id="home">
             <div className="title">
-              <img src="images/profile-selfie.jpg" alt="profile selfie"/>
+              <img src={profileSelfie} alt="profile selfie"/>
               <h1>Jonathan Howard</h1>
               <p>Software Developer</p>
             </div>
@@ -26,7 +38,7 @@ function App() {
             <div className="project">
               <a href="https://jonathanhhoward.github.io/survey-form">
                 <figure className="tile float">
-                  <img src="images/survey-form.png"
+                  <img src={surveyForm}
                        alt="icon for survey-form"/>
                   <figcaption className="caption">
                     Survey Form
@@ -35,7 +47,7 @@ function App() {
               </a>
               <a href="https://jonathanhhoward.github.io/technical-documentation-page">
                 <figure className="tile">
-                  <img src="images/technical-documentation-page.png"
+                  <img src={technicalDocumentationPage}
                        alt="icon for technical-documentation-page"/>
                   <figcaption className="caption">
                     Technical Document Page
@@ -44,7 +56,7 @@ function App() {
               </a>
               <a href="https://jonathanhhoward.github.io/product-landing-page/">
                 <figure className="tile">
-                  <img src="images/product-landing-page.png"
+                  <img src={productLandingPage}
                        alt="icon for product-landing-page"/>
                   <figcaption className="caption">
                     Product Landing Page
@@ -53,7 +65,7 @@ function App() {
               </a>
               <a href="https://jonathanhhoward.github.io/markdown-previewer">
                 <figure className="tile">
-                  <img src="images/markdown-previewer.png"
+                  <img src={markdownPreviewer}
                        alt="icon for markdown-previewer"/>
                   <figcaption className="caption">
                     Markdown Previewer
@@ -62,7 +74,7 @@ function App() {
               </a>
               <a href="https://jonathanhhoward.github.io/javascript-calculator">
                 <figure className="tile">
-                  <img src="images/javascript-calculator.png"
+                  <img src={javascriptCalculator}
                        alt="icon for javascript-calculator"/>
                   <figcaption className="caption">
                     JavaScript Calculator
@@ -71,7 +83,7 @@ function App() {
               </a>
               <a href="https://jonathanhhoward.github.io/pomodoro-clock">
                 <figure className="tile">
-                  <img src="images/pomodoro-clock.png"
+                  <img src={pomodoroClock}
                        alt="icon for pomodoro-clock"/>
                   <figcaption className="caption">
                     Pomodoro Clock
@@ -80,7 +92,7 @@ function App() {
               </a>
               <a href="https://jonathanhhoward.github.io/bar-chart">
                 <figure className="tile">
-                  <img src="images/bar-chart.png"
+                  <img src={barChart}
                        alt="icon for bar-chart"/>
                   <figcaption className="caption">
                     Bar Chart
@@ -89,7 +101,7 @@ function App() {
               </a>
               <a href="https://jhhoward-exercisetracker.glitch.me">
                 <figure className="tile">
-                  <img src="images/exercise-tracker.png"
+                  <img src={exerciseTracker}
                        alt="icon for exercise-tracker"/>
                   <figcaption className="caption">
                     Exercise Tracker
@@ -98,7 +110,7 @@ function App() {
               </a>
               <a href="https://jhhoward-filemetadata.glitch.me">
                 <figure className="tile">
-                  <img src="images/file-metadata.png"
+                  <img src={fileMetadata}
                        alt="icon for file-metadata"/>
                   <figcaption className="caption">
                     File Metadata Microservice
@@ -107,7 +119,7 @@ function App() {
               </a>
               <a href="https://jhhoward-metricimpconverter.glitch.me">
                 <figure className="tile">
-                  <img src="images/metric-imp-converter.png"
+                  <img src={metricImpConverter}
                        alt="icon for metric-imp-converter"/>
                   <figcaption className="caption">
                     Metric-Imperial Converter
@@ -116,7 +128,7 @@ function App() {
               </a>
               <a href="https://github.com/jonathanhhoward/clock">
                 <figure className="tile">
-                  <img src="images/analog-clock.png"
+                  <img src={analogClock}
                        alt="icon for analog-clock"/>
                   <figcaption className="caption">
                     Analog Clock
