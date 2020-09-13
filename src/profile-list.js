8c35c5b335d5fa7875e8eca19a656a50927083c5
jonathanhhoward <2773262@gmail.com>
2020-09-13 16:51:39 -0400

    Move file to src/

--- src/profile-list.js
+++ src/profile-list.js
@@ -1,9 +1,9 @@
-import githubLogo from '../images/GitHub_Logo.png';
-import githubMark from '../images/GitHub-Mark-64px.png';
-import fccLogo from '../images/fcc_secondary_large.png';
-import fccMark from '../images/fcc_secondary_small.png';
-import liLogo from '../images/LI-Logo_b&w.png';
-import liMark from '../images/LI-In-Bug_b&w.png';
+import githubLogo from './images/GitHub_Logo.png';
+import githubMark from './images/GitHub-Mark-64px.png';
+import fccLogo from './images/fcc_secondary_large.png';
+import fccMark from './images/fcc_secondary_small.png';
+import liLogo from './images/LI-Logo_b&w.png';
+import liMark from './images/LI-In-Bug_b&w.png';
 
 const profileList = [
   {
